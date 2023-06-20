993db360d0fdce3b24fa2af0310c528f98141d4f
A1CodeBreaker <129992490+A1CodeBreaker@users.noreply.github.com>
2023-06-19 23:28:31 -0400

    todo exercises

---
+++ json-placeholder/scripts/new-todo.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const addBtn = document.getElementById('addBtn');
+const titleInputEl = document.getElementById('title');
+const userIdInputEl = document.getElementById('userId');
+const messageEl = document.getElementById('message');
+
+addBtn.addEventListener('click', () => {
+  const todoValues = {
+    title: titleInputEl.value,
+    userId: +userIdInputEl.value,
+    completed: false
+  };
+
+  console.log(todoValues);
+  const url = 'http://localhost:3000/todos';
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(todoValues),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  };
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(newTodo => {
+      const message = `Created new Todo with ID ${newTodo.id}`;
+      messageEl.textContent = message;
+    });
+});
